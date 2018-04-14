d0b456b5e03fc391586422ba3ddda78a898babfb
marissake <ke.marissa@gmail.com>
2018-04-13 22:21:33 -0400

    calculator homework update

--- homework/calculator_box/js/script.js
+++ homework/calculator_box/js/script.js
@@ -1,17 +1,23 @@
 
-//commonly used variables
-var integer = parseInt(document.querySelector("#out").innerHTML, 10);
-var zeroBox = document.querySelector("#out");
+//variables
 
-//list of functions
+var zeroBox = document.querySelector("#out");
+var add10box = document.querySelector("#a10");
+var redBox = document.querySelector("#red");
+var blueBox = document.querySelector("#blue");
+var add20box = document.querySelector("#a20");
+var add30box = document.querySelector("#a30");
+var minus10box = document.querySelector("#n10");
+var minus20box = document.querySelector("#n20");
+var minus30box = document.querySelector("#n30");
+
+//functions
 
 function turnRed() {
-	var redBox = document.querySelector("#red");
 	zeroBox.style.backgroundColor = "red";
 }
 	
-function turnBlue() {
-	// var blueBox = document.querySelector("#blue"); //why won't the onclick accept my bluebox variable?
+function turnBlue() { 
 	zeroBox.style.backgroundColor = "blue";
 }
 
@@ -21,48 +27,42 @@ function reset() {
 }
 
 function add10() {
-	var add10box = document.querySelector("#a10");
 	var integer = parseInt(document.querySelector("#out").innerHTML, 10);
 	zeroBox.innerHTML = integer + 10;
 }
 
 function add20() {
-	var add20box = document.querySelector("#a20");
 	var integer = parseInt(document.querySelector("#out").innerHTML, 10);
 	zeroBox.innerHTML = integer + 20;
 }
 
 function add30() {
-	var add30box = document.querySelector("#a30");
 	var integer = parseInt(document.querySelector("#out").innerHTML, 10);
 	zeroBox.innerHTML = integer + 30;
 }
 
 function minus10() {
-	var minus10box = document.querySelector("#n10");
 	var integer = parseInt(document.querySelector("#out").innerHTML, 10);
 	zeroBox.innerHTML = integer - 10;
 }
 
 function minus20() {
-	var minus20box = document.querySelector("#n20");
 	var integer = parseInt(document.querySelector("#out").innerHTML, 10);
 	zeroBox.innerHTML = integer - 20;
 }
 
 function minus30() {
-	var minus20box = document.querySelector("#n30");
 	var integer = parseInt(document.querySelector("#out").innerHTML, 10);
 	zeroBox.innerHTML = integer - 30;
 }
 
 //click events
-document.querySelector("#red").onclick = turnRed;
-document.querySelector("#blue").onclick = turnBlue;
-document.querySelector("#out").onclick = reset;
-document.querySelector("#a10").onclick = add10; //why won't the onclick accept my add10box variable(above)?
-document.querySelector("#a20").onclick = add20;
-document.querySelector("#a30").onclick = add30;
-document.querySelector("#n10").onclick = minus10;
-document.querySelector("#n20").onclick = minus20;
-document.querySelector("#n30").onclick = minus30;
+redBox.onclick = turnRed;
+blueBox.onclick = turnBlue;
+zeroBox.onclick = reset;
+add10box.onclick = add10;
+add20box.onclick = add20;
+add30box.onclick = add30;
+minus10box.onclick = minus10;
+minus20box.onclick = minus20;
+minus30box.onclick = minus30;
