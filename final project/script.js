830d099e7cab1136067246d6e89d9e44cf261d90
marissake <ke.marissa@gmail.com>
2018-04-27 16:54:01 -0400

    final proj update

--- final project/script.js
+++ final project/script.js
@@ -45,7 +45,7 @@ $(window).on("load",function() {
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade").each(function() {
-      /* Check the location of each desired element */
+       Check the location of each desired element 
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
