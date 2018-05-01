29d735332f1a4207cf4b1633df626e5afb723def
marissake <ke.marissa@gmail.com>
2018-05-01 09:56:11 -0400

    final proj update

--- final project/script.js
+++ final project/script.js
@@ -1,51 +1,13 @@
-// $(document).ready(function() {
-//     (function() {
-//         //settings
-//         var fadeSpeed = 200, fadeTo = 0.5, topDistance = 30;
-//         var topbarME = function() { $('#uberbar').fadeTo(fadeSpeed,1); }, topbarML = function() { $('#uberbar').fadeTo(fadeSpeed,fadeTo); };
-//         var inside = false;
-//         //do
-//         $(window).scroll(function() {
-//             position = $(window).scrollTop();
-//             if(position > topDistance && !inside) {
-//                 //add events
-//                 topbarML();
-//                 $('#uberbar').bind('mouseenter',topbarME);
-//                 $('#uberbar').bind('mouseleave',topbarML);
-//                 inside = true;
-//             }
-//             else if (position < topDistance){
-//                 topbarME();
-//                 $('#uberbar').unbind('mouseenter',topbarME);
-//                 $('#uberbar').unbind('mouseleave',topbarML);
-//                 inside = false;
-//             }
-//         });
-//     })();
-// });
 
-
-
-function fadein() {
-    $(.overlay).fadeIn("slow");
-}
-
-fadein();
-
-// var header = $(".header");
-var header = $('header');
-$(window).scroll(function() {
-    header.addClass('lower-opacity');
-    if($(this).scrollTop() < 40) {
-        header.removeClass('lower-opacity');
-    }
+$(document).ready(function () {
+    $('.overlay').fadeIn(1000).removeClass('hidden');
 });
 
 $(window).on("load",function() {
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade").each(function() {
-       Check the location of each desired element 
+      /* Check the location of each desired element */
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
@@ -56,4 +18,12 @@ $(window).on("load",function() {
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
+});
+
+//scroll to anchor point when clicking anchor tag
+$("a[href^=#]").click(function(e) { 
+  e.preventDefault(); 
+  var dest = $(this).attr('href'); 
+  console.log(dest); 
+  $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow'); 
 });
